7d5ba732c18d49be0a50b0235fd6b09470f75729
Oscar Bernal <odbg2013@gmail.com>
2022-06-06 23:18:14 -0500

    carousel w/API

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
+const posterWidth300 = "https://image.tmdb.org/t/p/w300";
 
+// Get trending movies preview
+async function getTrendingMoviesPreview() {
+  const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
 
-console.log("Hi");
+  const data = await res.json();
+  const movies = data.results;
+
+  // Creates the carousel section
+  movies.forEach((movie) => {
+    const sectionCarousel = document.getElementById("carousel");
+    const movieImg = document.createElement("img");
+
+    movieImg.setAttribute("alt", movie.title);
+    movieImg.setAttribute("src", `${posterWidth300}/${movie.poster_path}`);
+
+    sectionCarousel.appendChild(movieImg);
+  });
+}
+
+getTrendingMoviesPreview();
