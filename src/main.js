da8a05ebf64476a1c81bf89c0c4723722cd0f046
Oscar Bernal <57721146+maat-mind@users.noreply.github.com>
2022-06-08 12:46:28 -0500

    genres

--- src/main.js
+++ src/main.js
@@ -10,13 +10,34 @@ async function getTrendingMoviesPreview() {
   // Creates the carousel section
   movies.forEach((movie) => {
     const sectionCarousel = document.getElementById("carousel");
+    const divImg = document.createElement("div");
     const movieImg = document.createElement("img");
+
+    divImg.setAttribute("class", "carousel-item");
 
     movieImg.setAttribute("alt", movie.title);
     movieImg.setAttribute("src", `${posterWidth300}/${movie.poster_path}`);
 
-    sectionCarousel.appendChild(movieImg);
+    divImg.appendChild(movieImg);
+    sectionCarousel.appendChild(divImg);
+  });
+}
+
+// Get the common genres section preview
+async function getGenresPreview() {
+  const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+
+  const data = await res.json();
+  const genres = data.genres;
+
+  genres.forEach((genre) => {
+    const sectionGenresPreview = document.getElementById("common-genres");
+    const genreButton = document.createElement("button");
+
+    genreButton.innerHTML = `${genre.name}`;
+    sectionGenresPreview.appendChild(genreButton);
   });
 }
 
 getTrendingMoviesPreview();
+getGenresPreview();
